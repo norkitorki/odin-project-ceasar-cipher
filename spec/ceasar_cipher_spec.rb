543827fceee8afa17abdaf9910866b90de6c116a
norkitorki <norki@tuta.io>
2021-09-20 11:45:34 +0200

    Update rspec tests

--- spec/ceasar_cipher_spec.rb
+++ spec/ceasar_cipher_spec.rb
@@ -1,31 +1,35 @@
-require '../ceasar_cipher.rb'
+require_relative '../lib/ceasar_cipher.rb'
 
-describe "#ceasar_cipher" do
-  it "works with small positive integer shifts" do
-    expect(ceasar_cipher("ceasar", 5)).to eq("hjfxfw")
+describe 'ceasar_cipher' do
+  it 'should work with small integer shifts' do
+    expect(ceasar_cipher('ceasar', 5)).to eq('hjfxfw')
   end
 
-  it "works with big positive integer shifts" do
-    expect(ceasar_cipher("ceasar", 50244)).to eq("oqmemd")
+  it 'should work with big integer shifts' do
+    expect(ceasar_cipher('ceasar', 50244)).to eq('oqmemd')
   end
 
-  it "works with small negative integer shifts" do
-    expect(ceasar_cipher("ceasar", -5)).to eq("xzvnvm")
+  it 'should work with small negative integer shifts' do
+    expect(ceasar_cipher('ceasar', -5)).to eq('xzvnvm')
   end
 
-  it "works with big negative integer shifts" do
-    expect(ceasar_cipher("ceasar", -50244)).to eq("qsogof")
+  it 'should work with big negative integer shifts' do
+    expect(ceasar_cipher('ceasar', -50244)).to eq('qsogof')
   end
 
-  it "works with uppercase letters" do
-    expect(ceasar_cipher("CeasarCipher", 19)).to eq("VxtltkVbiaxk")
+  it 'should work with uppercase letters' do
+    expect(ceasar_cipher('CeasarCipher', 19)).to eq('VxtltkVbiaxk')
   end
 
-  it "works with punctuation" do
-    expect(ceasar_cipher("Ceasar Cipher!", 54)).to eq("Egcuct Ekrjgt!")
+  it 'should work with punctuation' do
+    expect(ceasar_cipher('Ceasar Cipher!', 54)).to eq('Egcuct Ekrjgt!')
   end
 
-  it "works with newline and tab characters" do
+  it 'should work with newline and tab characters' do
     expect(ceasar_cipher("Ceasar\n\tCipher", -103)).to eq("Dfbtbs\n\tDjqifs")
+  end
+
+  it 'should raise an error if passed in arguments are invalid' do
+    expect { ceasar_cipher([20], '5') }.to raise_error(ArgumentError)
   end
 end
