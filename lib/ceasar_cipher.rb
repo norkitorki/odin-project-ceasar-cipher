184dff4794b137543804e1b8ede961822f141524
norkitorki <norki@tuta.io>
2021-09-21 11:46:18 +0200

    Move cipher functionality into its own method

--- lib/ceasar_cipher.rb
+++ lib/ceasar_cipher.rb
@@ -1,16 +1,19 @@
 # frozen-string-literal: true
 
+def cipher(letter, shift)
+  alphabet = ('a'..'z').to_a
+  codepoint = letter.ord
+  if codepoint.between?(97, 122)
+    alphabet[(codepoint % 97 + shift) % 26]
+  elsif codepoint.between?(65, 90)
+    alphabet[(codepoint % 65 + shift) % 26].upcase
+  else
+    codepoint.chr
+  end
+end
+
 def ceasar_cipher(string, shift)
   raise ArgumentError unless string.is_a?(String) && shift.is_a?(Integer)
-  
-  alphabet = ('a'..'z').to_a
-  string.codepoints.map! do |c|
-    if c.between?(97, 122)
-      alphabet[(c % 97 + shift) % 26]
-    elsif c.between?(65, 90)
-      alphabet[(c % 65 + shift) % 26].upcase
-    else
-      c.chr
-    end
-  end.join
+
+  string.chars.map { |c| cipher(c, shift) }.join
 end
